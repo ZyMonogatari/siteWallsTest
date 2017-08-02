4757aef9cccb46b3591732e21ec0f2e9720c1ffc
Zombiee K.S <Zombiee K.S>
2017-08-02 18:13:15 -0500

    final yael

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -87,9 +87,6 @@ angular.module('application').controller('contactoCtrl',
             
           $scope.cabecera.top = '5%';
           $scope.logoTransform = 'scale(1, 1)';
-          $scope.cabecera.fontColor = 'white';
-          $scope.iconColor = 'white';
-          $scope.cabecera.source = '/assets/img/logo.png';
           $scope.$apply();
         }
         if((document.getElementById('contact-container-BounceIn').getBoundingClientRect().top + 00) <= screen.height){
@@ -238,6 +235,7 @@ angular.module('application').controller('franquiciasCtrl',
 angular.module('application').controller('galeriaCtrl',
   ['$scope', '$window', 'FoundationApi', '$sce', '$state', function($scope, $window, FoundationApi, $sce, $state){
     document.title = "Galería - Wall´s Barbershop";
+    var galleryIndex;
     $scope.cabecera = {};
     $scope.displaySesionN = 'none';
     $scope.cabecera.source = '/assets/img/wallslogo-negro-min.png';
@@ -247,6 +245,8 @@ angular.module('application').controller('galeriaCtrl',
     $scope.displaySesion = 'none';
     $scope.displayNumber = 'none';
     $scope.iconColor = '#333';
+    $scope.leftArrow = true;
+    $scope.rightArrow = true;
     $scope.go = function(state){
         window.scrollTo(0, 0)
         $state.go(state);
@@ -266,20 +266,37 @@ angular.module('application').controller('galeriaCtrl',
     $scope.disappearItem = function(item){
       angular.element(document.querySelector('#' + item)).addClass('disappear1S');
     }
+    $scope.fullGallery = [
+      '/assets/img/galeria/1.jpg',
+      '/assets/img/galeria/3.jpg',
+      '/assets/img/galeria/2.jpg',
+      '/assets/img/galeria/9.jpg',
+      '/assets/img/galeria/4.jpg',
+      '/assets/img/galeria/7.jpg',
+      '/assets/img/galeria/6.jpg',
+      '/assets/img/galeria/13.jpg',
+      '/assets/img/galeria/5.jpg',
+      '/assets/img/galeria/8.jpg',
+      '/assets/img/galeria/20.jpg',
+      '/assets/img/galeria/21.jpg', 
+      '/assets/img/galeria/12.jpg',
+      '/assets/img/galeria/24.jpg',
+      '/assets/img/galeria/10.jpg'
+    ];
     $scope.gallery = {
       col1: [
         '/assets/img/galeria/1.jpg',
         '/assets/img/galeria/3.jpg',
         '/assets/img/galeria/2.jpg',
         '/assets/img/galeria/9.jpg',
-        '/assets/img/galeria/4.jpg',
+        '/assets/img/galeria/4.jpg'
       ],
       col2: [
         '/assets/img/galeria/7.jpg',
         '/assets/img/galeria/6.jpg',
         '/assets/img/galeria/13.jpg',
         '/assets/img/galeria/5.jpg',
-        '/assets/img/galeria/8.jpg',
+        '/assets/img/galeria/8.jpg'
       ],
       col3: [
         '/assets/img/galeria/20.jpg',
@@ -291,9 +308,38 @@ angular.module('application').controller('galeriaCtrl',
     };
     $scope.openModal = function(image){
       $scope.actualImage = image;
+      $scope.rightArrow = true;
+      $scope.leftArrow = true;
+      galleryIndex = $scope.fullGallery.indexOf(image);
+      if(galleryIndex == 0){
+        $scope.leftArrow = false;
+      }
+      if(($scope.fullGallery.length - 1) == galleryIndex){
+        $scope.rightArrow = false;
+      }
     }
       
-      
+    $scope.prevImg = function(){
+      $scope.rightArrow = true;
+      if(galleryIndex > 0){
+        galleryIndex--;
+        $scope.actualImage = $scope.fullGallery[galleryIndex];
+      }
+      if(galleryIndex == 0){
+        $scope.leftArrow = 0;
+      }
+    }
+
+    $scope.nextImg = function(){
+      $scope.leftArrow = true;
+      if(galleryIndex < ($scope.fullGallery.length - 1)){
+        galleryIndex++;
+        $scope.actualImage = $scope.fullGallery[galleryIndex];
+      }
+      if(galleryIndex == ($scope.fullGallery.length - 1)){
+        $scope.rightArrow = false;
+      }
+    }
     $scope.mostrarMenu = function (){
             console.log($scope.displayMenu);
 
@@ -326,20 +372,17 @@ angular.module('application').controller('galeriaCtrl',
       }
     if((document.getElementById('cuerpo1').getBoundingClientRect().top -50)  <= 0 & (document.getElementById('cuerpo1').getBoundingClientRect().top) >= -1000){
       $scope.actualBody = 2;
-      console.log("pequeño" + " - " + document.getElementById('cuerpo1').getBoundingClientRect().top);
       $scope.$apply();
     } else if((document.getElementById('cuerpo1').getBoundingClientRect().top -50)  >= 0){
       $scope.actualBody = 1;
-      console.log("head")
       $scope.$apply();
     }
     if((document.getElementById('cuerpo2').getBoundingClientRect().top -50)  <= 0){
-      console.log("video")
       $scope.actualBody = 3;
       $scope.$apply();
     }
     for(var i = 0; i < document.getElementsByClassName('galleryBox').length; i++){
-      if((document.getElementsByClassName('galleryBox')[i].getBoundingClientRect().top + 50) <= screen.height){
+      if((document.getElementsByClassName('galleryBox')[i].getBoundingClientRect().top + 50) <= screen.height & screen.width > 768){
           angular.element(document.getElementsByClassName('galleryBox')[i]).addClass('appear');
       }
     }
@@ -348,6 +391,15 @@ angular.module('application').controller('galeriaCtrl',
 
 angular.module('application').controller('homeCtrl',
   ['$scope', '$window', 'NgMap', '$state',  function($scope, $window, NgMap, $state){
+    
+    window.mobileAndTabletcheck = function() {
+     var check = false;
+     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+     return check;
+   };
+
+   $scope.mobile = window.mobileAndTabletcheck();
+   console.log($scope.mobile);
     $scope.mostrarMenu = function (){
         if($scope.displayMenu == 'initial'){
             $scope.displayMenu = 'none';
@@ -356,10 +408,18 @@ angular.module('application').controller('homeCtrl',
             $scope.displayMenu = 'initial';
         }
     }
+
     $scope.displaySesionN = 'none';
-    $scope.goMap = function(e, state){
-      $state.go(state).then(function(){
-           window.location.reload(true); 
+
+    $scope.goMap = function(){
+      console.log("click de sucursal")
+      window.scrollTo(0,0);      
+      $state.go('sucursales').then(function(){
+        $window.location.reload();
+        var top =  document.getElementById('map').getBoundingClientRect().top
+        console.log(top);
+          window.scrollTo(0, top);
+         
         });
     }
     $scope.go = function(state){
@@ -400,7 +460,7 @@ angular.module('application').controller('homeCtrl',
 
    
     if(screen.width <= 768){
-      $scope.zoom = 5;
+      $scope.zoom = 4;
     }
     else{
       $scope.zoom = 6;
@@ -662,7 +722,12 @@ angular.module('application').controller('sucursalesCtrl',
         $scope.mobil = true;
 
     }
-    else{
+    if(screen.width <= 414){
+        $scope.menuPosition = "10.000000, -98.7000000";
+        $scope.mobil = true;
+
+    }
+    if(screen.width > 768){
         $scope.menuPosition = "32.400000, -96.6000000";
         $scope.mobil = false;
     }
@@ -824,7 +889,10 @@ angular.module('application').controller('sucursalesCtrl',
         console.log(map);
     });
     
-
+    $scope.return = function(){
+        $scope.sucursal = false;
+        $scope.actualSucursal = null;
+    }
     $scope.selectCity = function(city){
         $scope.sucursalesColumna1 = {};
         $scope.sucursalesColumna2 = {};
@@ -1397,7 +1465,7 @@ angular.module('application').controller('sucursalesCtrl',
             direccion: 'SM. 56 MZA. 1 Lote 55, Av. Prolongación, Luis Donaldo Colosio, Fracc. Santa Fé, Playa del Carmen, México.',
             contacto: 'WhatsApp. (984) 187 6258',
             position: '25.000000, -85.0000000',
-            img: '/assets/img/sucursales/walls-playa.png',
+            img: '/assets/img/sucursales/playacentro.jpg',
             fb: 'https://www.facebook.com/wallsplayacolosio/?ref=br_rs', 
             instagram : 'https://www.instagram.com/wallsbarbershop_playa/'
         }, 
@@ -1407,7 +1475,7 @@ angular.module('application').controller('sucursalesCtrl',
             direccion: '15 Avenida norte, entre calle 8 y calle 10, Colonia centro, plaza pelícanos, Playa del Carmen, Quintana Roo, México.',
             contacto: 'Tel. (984) 803 3742 y WhatsApp. (984) 142 3333',
             position: '16.800000, -85.0000000',
-            img: '/assets/img/sucursales/walls-playa.png',
+            img: '/assets/img/sucursales/playa.png',
             fb: 'https://www.facebook.com/wallsbarbershopplayadelcarmen/', 
             instagram : 'https://www.instagram.com/wallsbarbershop_playa/'
         },
@@ -1467,7 +1535,7 @@ angular.module('application').controller('sucursalesCtrl',
             direccion: 'Plaza Santa Fe, local 38, Av. Universidad #811, Bosques del prado sur, Aguascalientes, México.',
             contacto: 'Tel. 449 288 1015 y WhatsApp. 449 196 1237',
             position: '16.800000, -85.0000000',
-            img: '/assets/img/sucursales/walls-aguascalientes.png',
+            img: '/assets/img/sucursales/Aguascalienstes.jpg',
             fb: 'https://www.facebook.com/wallsbarbershopags/', 
             instagram : 'https://www.instagram.com/wallsbarbershop_ags/'
         },
@@ -1573,7 +1641,7 @@ angular.module('application').controller('sucursalesCtrl',
             direccion: 'Nicolás Bravo S/N Centro, Isla Mujeres.',
             contacto: 'Tel. 877 0496 y WhatsApp. (998) 117 1259',
             position: '16.800000, -85.0000000',
-            img: '/assets/img/sucursales/IslaMujeres.jpg',
+            img: '/assets/img/sucursales/Isla.jpg',
             fb: 'https://www.instagram.com/wallschetumal/', 
             instagram : 'https://www.instagram.com/wallsbarbershop_isla/'
 
@@ -1594,7 +1662,7 @@ angular.module('application').controller('sucursalesCtrl',
             direccion: 'SM. 56 MZA. 1 Lote 55, Av. Prolongación, Luis Donaldo Colosio, Fracc. Santa Fé, Playa del Carmen, México.',
             contacto: 'WhatsApp. (984) 187 6258',
             position: '25.000000, -85.0000000',
-            img: '/assets/img/sucursales/walls-playa.png',
+            img: '/assets/img/sucursales/playacentro.jpg',
             fb: 'https://www.facebook.com/wallsplayacolosio/?ref=br_rs', 
             instagram : 'https://www.instagram.com/wallsbarbershop_playa/'
 
@@ -1605,7 +1673,7 @@ angular.module('application').controller('sucursalesCtrl',
             direccion: '15 Avenida norte, entre calle 8 y calle 10, Colonia centro, plaza pelícanos, Playa del Carmen, Quintana Roo, México.',
             contacto: 'Tel. (984) 803 3742 y WhatsApp. (984) 142 3333',
             position: '16.800000, -85.0000000',
-            img: '/assets/img/sucursales/walls-playa.png',
+            img: '/assets/img/sucursales/playa.png',
             fb: 'https://www.facebook.com/wallsbarbershopplayadelcarmen/', 
             instagram : 'https://www.instagram.com/wallsbarbershop_playa/'
         },
@@ -1637,7 +1705,7 @@ angular.module('application').controller('sucursalesCtrl',
             direccion: 'Plaza Santa Fe, local 38, Av. Universidad #811, Bosques del prado sur, Aguascalientes, México.',
             contacto: 'Tel. 449 288 1015 y WhatsApp. 449 196 1237',
             position: '16.800000, -85.0000000',
-            img: '/assets/img/sucursales/walls-aguascalientes.png',
+            img: '/assets/img/sucursales/Aguascalienstes.jpg',
             fb: 'https://www.facebook.com/wallsbarbershopbocadelrio/', 
             instagram : 'https://www.instagram.com/wallsbarbershop_boca/'
         },
@@ -1877,7 +1945,7 @@ angular.module('application').controller('sucursalesCtrl',
 }]);
 angular.module('application').controller('wallsCardCtrl',
   ['$scope', '$window', '$state', '$dbApi', 'FoundationApi', function($scope, $window,  $state, $dbApi, FoundationApi){
-  	$scope.screenHeight = (screen.height - 50) + 'px';
+  	$scope.screenHeight = '720px';
   	console.log($scope.screenHeight);
   	$scope.displaySesionN = 'none';
     angular.element(document.querySelector('#selloBounce2s')).addClass('bounceIn2s');
